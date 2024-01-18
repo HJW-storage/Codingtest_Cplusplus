f4322a6403aabea4302109999d5fce4580451778
Hong Ji Whan <h1234p@naver.com>
2024-01-18 15:49:11 +0900

    2024-01-17

--- Big_stone/3week/16637.cpp
+++ Big_stone/3week/16637.cpp
@@ -9,6 +9,8 @@ vector<int> res;
 const int idx[10] = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18 };
 int n = 0;
 string susik;
+
+// 조합 잘 뽑히는지 확인하려는 함수
 void printV(vector<int>& v) {
 	for (int it : v) {
 		cout << it << " ";
@@ -16,6 +18,7 @@ void printV(vector<int>& v) {
 	cout << "\n";
 	return;
 }
+// 연산자별 연산 함수
 int operator_part(int i, char op, string& check_str) {
 	if (op == '+') {
 		return (check_str[i + 1] - 48) + (check_str[i + 3] - 48);	// 덧셈처리
@@ -27,22 +30,27 @@ int operator_part(int i, char op, string& check_str) {
 		return (check_str[i + 1] - 48) * (check_str[i + 3] - 48);
 	}
 }
+// 괄호를 추가하여 완성된 string을 괄호로 쌓여있는 계산 먼저 다하고 string vector에 저장하고
+// 이후 계산하는 함수.
 void cal_str(string& check_str) {
 	vector<string> vec_str;
 	for (int i = 0; i < check_str.size(); i++) {
+		// 괄호가 되어 있는 부분만 우선적으로 연산을 하고 string vector에 저장. 
 		if (check_str[i] == '(') {
 			string ttmp = to_string((operator_part(i, check_str[i + 2], check_str)));
 			vec_str.push_back(ttmp);
 			i += 4;
 		}
+		// 이외의 것들은 string vector에 그대로 저장. 
 		else {
 			vec_str.push_back(string(1, check_str[i]));	// char를 string으로 형변환.
 		}
 	}
 
+	// 괄호로 쌓여진 연산이 계산되서 저장 되어 있기에 왼쪽부터 오른쪽 순서대로 연산하면 된다.
 	//for (string it : vec_str) cout << it << " ";
 	//cout << "\n";
-	int res_o = stoi(vec_str[0]);
+	int res_o = stoi(vec_str[0]);	// string을 int로 변환하는 함수. stoi()
 	for (int i = 0; i < vec_str.size(); i++) {
 		if (vec_str[i] == "+") {
 			res_o = res_o + stoi(vec_str[i + 1]);	// 덧셈처리	string을 int로 형변환.
@@ -67,25 +75,27 @@ void cal_str(string& check_str) {
 	res.push_back(res_o);
 	return;
 }
+// 조합 구현. 인덱스 기반으로 조합을 뽑는 것!! 
 void combi(int start, int n_len, int r, vector<int> &v) {
 	if (v.size() == r) {
 		// 로직 구현.
 		bool check_flag = true;
-		int prev_it = 1000;
+		int prev_it = 1000;	
 		for (int i = 0; i < v.size(); i++) {
 			// 괄호를 열고 닫는 것을 고려하면 겹치는 부분을 제외해야하기에 idx 2이상 차이가 나게 뽑아햐한다.
+			// 예를 들어, (3+(8)*7)-9*2 <- idx 차이가 1일 때, 문제 조건상 오류에 해당. 
 			if (v[i] - prev_it == 1) {
 				check_flag = false;
 				break;
 			}
-			prev_it = v[i];
+			prev_it = v[i];	// 이전 값과 비교를 위한 prev 저장. 
 		}
 		
 		if (check_flag == true) {
-			// 괄호 삽입하기. 
 			//printV(v);
-			string check_str = susik;
+			string check_str = susik;	// str 복사. 
 			int cnt = 0;
+			// 괄호 삽입하기. 
 			for (int it : v) {
 				check_str.insert(check_str.begin() + idx[it] + cnt, '(');
 				cnt++;
@@ -95,7 +105,7 @@ void combi(int start, int n_len, int r, vector<int> &v) {
 			/*printV(v);*/
 			//cout << "괄호 추가한 string : " << check_str;
 			//cout << "\n";
-			cal_str(check_str);
+			cal_str(check_str);	// 괄호가 추가된 수식 계산. 
 		}
 		else { ; }
 		return;
@@ -162,9 +172,12 @@ int main() {
 		combi(-1, n_len, i, b);
 
 	}
+	// 최댓값 출력을 위한 오름차순 정렬.
 	sort(res.begin(), res.end());
 	//for (auto it : res) cout << it << " ";
 	//cout << "\n";
+
+	// 최댓값 출력
 	cout << * (res.end() - 1) << "\n";
 	return 0;
 }
